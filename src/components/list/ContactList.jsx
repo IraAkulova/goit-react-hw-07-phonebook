d0a4b0cc626c45c85d9d0f7d6586b3dd5c0e8bbf
Ira Akulova <ira.akulova2019@gmail.com>
2023-04-19 10:38:59 +0300

    done

--- src/components/list/ContactList.jsx
+++ src/components/list/ContactList.jsx
@@ -1,22 +1,11 @@
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../redux/operations';
-import { getContacts, getFilter } from 'redux/selectors';
+import { getVisibleContacts } from 'redux/selectors';
 
-export const ContactList = ({contact}) => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
+export const ContactList = () => {
   const dispatch = useDispatch();
-
-  const getVisibleContacts = () => {
-      const normalizedFilter = filter.toLocaleLowerCase();
-      return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizedFilter)
-      );
-    };
-  const visibleContacts = getVisibleContacts();
-
+  const visibleContacts = useSelector(getVisibleContacts);
   const handleDelete = id => dispatch(deleteContact(id));
 
   return (
